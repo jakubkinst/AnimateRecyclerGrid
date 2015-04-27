7a0718a758b76eb340868bfb90716f0af1819a57
Michael Lustig <halfjew22@gmail.com>
2015-04-27 18:56:11 -0400

    Figured out ViewTreeObserver tells me when the view is layed out.

--- app/src/main/java/com/lustig/autofitgridrecyclerview/activities/MainActivity.java
+++ app/src/main/java/com/lustig/autofitgridrecyclerview/activities/MainActivity.java
@@ -2,10 +2,13 @@ package com.lustig.autofitgridrecyclerview.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.GridLayoutManager;
+import android.util.Log;
+import android.view.ViewTreeObserver;
 
 import com.lustig.autofitgridrecyclerview.R;
 import com.lustig.autofitgridrecyclerview.adapters.NumberedAdapter;
+import com.lustig.autofitgridrecyclerview.recyclers.AutoFitRecyclerView;
 
 public class MainActivity extends Activity {
 
@@ -14,8 +17,29 @@ public class MainActivity extends Activity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        final AutoFitRecyclerView recyclerView = (AutoFitRecyclerView) findViewById(R.id.recycler_view);
         recyclerView.setHasFixedSize(true);
+
         recyclerView.setAdapter(new NumberedAdapter(100));
+
+        Log.d("MainActivity", "end of onCreate");
+
+        final GridLayoutManager manager = (GridLayoutManager) recyclerView.getLayoutManager();
+
+        ViewTreeObserver vto = recyclerView.getViewTreeObserver();
+        vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+
+                                          @Override
+                                          public void onGlobalLayout() {
+
+                                              Log.d("MainActivity", "" + manager.findFirstCompletelyVisibleItemPosition());
+                                              Log.d("MainActivity", "" + manager.findLastCompletelyVisibleItemPosition());
+
+                                              recyclerView.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+                                          }
+                                      });
+
+
     }
+
 }
--- app/src/main/java/com/lustig/autofitgridrecyclerview/adapters/NumberedAdapter.java
+++ app/src/main/java/com/lustig/autofitgridrecyclerview/adapters/NumberedAdapter.java
@@ -1,7 +1,9 @@
 package com.lustig.autofitgridrecyclerview.adapters;
 
 import android.support.v7.widget.CardView;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +11,7 @@ import android.widget.TextView;
 
 import com.lustig.autofitgridrecyclerview.R;
 import com.lustig.autofitgridrecyclerview.animations.AnimationHelper;
+import com.lustig.autofitgridrecyclerview.recyclers.AutoFitRecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,13 +21,49 @@ public class NumberedAdapter extends RecyclerView.Adapter<NumberedAdapter.TextVi
     private List<String> labels;
     private AnimationHelper mHelper;
 
+    private AutoFitRecyclerView mRecyclerView;
+
+    private int mColumns = -1;
+    private int mRows = -1;
+
+    private GridLayoutManager mManager;
+
     public NumberedAdapter(int count) {
+
+        Log.d("Lustig", "begin NumberAdapter constructor");
 
         mHelper = new AnimationHelper();
 
         labels = new ArrayList<String>(count);
         for (int i = 0; i < count; ++i) {
             labels.add(String.valueOf(i));
+        }
+
+        Log.d("Lustig", "end NumberAdapter constructor");
+
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+
+        Log.d("Lustig", "begin onAttached");
+
+        super.onAttachedToRecyclerView(recyclerView);
+        mRecyclerView = (AutoFitRecyclerView) recyclerView;
+        mManager = ((GridLayoutManager) mRecyclerView.getLayoutManager());
+
+        Log.d("Lustig", "end onAttached");
+    }
+    public class TextViewHolder extends RecyclerView.ViewHolder {
+
+        public CardView cardView;
+        public TextView textView;
+
+        public TextViewHolder(View itemView) {
+
+            super(itemView);
+            cardView = (CardView) itemView.findViewById(R.id.card);
+            textView = (TextView) itemView.findViewById(R.id.text);
         }
     }
 
@@ -38,29 +77,55 @@ public class NumberedAdapter extends RecyclerView.Adapter<NumberedAdapter.TextVi
     @Override
     public void onBindViewHolder(final TextViewHolder holder, final int position) {
 
+        Log.d("Lustig", "begin onBindViewHolder");
+
+        Log.d("Lustig", "member columsn: " + mColumns);
+
+        Log.d("Lustig", "first visible " + mManager.findFirstVisibleItemPosition());
+        Log.d("Lustig", "last completely visible" + mManager.findLastCompletelyVisibleItemPosition());
+
+
+        Log.d("Lustig", "columns: " + mRecyclerView.getNumColumns() + "");
+
+        /* If mColumns has been set, the code will skip this block */
+        if (mColumns == -1) {
+
+            Log.d("Lustig", "setting adapter numColumns");
+
+//
+//
+//            int firstVisiblePosition =
+//            int lastVisiblePosition = manager.findLastCompletelyVisibleItemPosition();
+//
+//            /* Test to see if we get the right height */
+//            int height = lastVisiblePosition - firstVisiblePosition;
+//
+////            Log.d("Lustig", "is the height " + height + "?");
+
+
+            mColumns = mRecyclerView.getNumColumns();
+
+            Log.d("Lustig", "mColumns now = " + mColumns);
+        }
+
         final String label = labels.get(position);
         holder.textView.setText(label);
 
         mHelper.addViewToQueue(holder.cardView);
+
+        Log.d("Lustig", "columns: " + mRecyclerView.getNumColumns() + "");
+
+        Log.d("Lustig", "end onBindViewHolder");
 
     }
 
     @Override
     public int getItemCount() {
 
+        Log.d("Lustig", "getItemCount");
         return labels.size();
+
+
     }
 
-    public class TextViewHolder extends RecyclerView.ViewHolder {
-
-        public CardView cardView;
-        public TextView textView;
-
-        public TextViewHolder(View itemView) {
-
-            super(itemView);
-            cardView = (CardView) itemView.findViewById(R.id.card);
-            textView = (TextView) itemView.findViewById(R.id.text);
-        }
-    }
 }

a850eea370378219132940d40b4ddc61c2af6647
Michael Lustig <halfjew22@gmail.com>
2015-05-04 17:20:12 -0400

    Fixed issue #1

--- app/src/main/java/com/lustig/autofitgridrecyclerview/animations/AnimationHelper.java
+++ app/src/main/java/com/lustig/autofitgridrecyclerview/animations/AnimationHelper.java
@@ -33,7 +33,12 @@ import com.lustig.autofitgridrecyclerview.recyclers.AutoFitRecyclerView;
 
 public class AnimationHelper implements OnChildAttachedListener {
 
+    /**
+     * This is the time that it takes one cell to complete an animation.
+     */
     public static final long DEFAULT_DURATION_MS = 350;
+
+    public static final long DEFAULT_DEBUG_DURATION_MS = 1250;
 
     private static final String DEFAULT_PROPERTY_TO_ANIMATE = "alpha";
 
@@ -41,7 +46,7 @@ public class AnimationHelper implements OnChildAttachedListener {
     private String mViewPropertyToAnimate = DEFAULT_PROPERTY_TO_ANIMATE;
 
     /* Duration of animations, set to default value, but can be changed */
-    private long mAnimationDuration = DEFAULT_DURATION_MS;
+    private long mAnimationDuration = DEFAULT_DEBUG_DURATION_MS;
 
     /* Length of time to wait before spawning next wave of animations */
     private long mDeltaT = (long) (mAnimationDuration / 3.5);
@@ -59,14 +64,6 @@ public class AnimationHelper implements OnChildAttachedListener {
 
     private int mNumChildren = -1;
 
-    /* Scroll tracking variables */
-    private int mTotalYScroll = 0;
-
-    private int mTotalHeight = -1;
-
-    /* I believe this should remain constant once set. Double check though */
-    private int mRowHeight = -1;
-
     private int mPreviousLastVisibleChildPosition = -1;
 
     private int mCurrentLastVisibleChildPosition = -1;
@@ -78,6 +75,13 @@ public class AnimationHelper implements OnChildAttachedListener {
     private GridLayoutManager mManager;
 
     private boolean mFirstAnimationsCompleted = false;
+
+    private long mAnimStartTime;
+    private long mAnimEndTime;
+
+    private long mTimeToCompleteScreenAnimation;
+
+    private long mTimeToCompleteTotalAnimation;
 
     /**
      * This one is called by MainActivity in order to pass it to the RecyclerView
@@ -110,8 +114,14 @@ public class AnimationHelper implements OnChildAttachedListener {
 
         mManager = (GridLayoutManager) mRecyclerView.getLayoutManager();
 
+        /**
+         * This onGlobalLayoutListener listens for when the layout has been completed and I can
+         * start the animation. It then gathers all of the views that can be animated and starts
+         * the animation. I need to break up this code into smaller, more understandable chunks.
+         */
         mRecyclerView.getViewTreeObserver().addOnGlobalLayoutListener(
                 new ViewTreeObserver.OnGlobalLayoutListener() {
+
                     @Override
                     public void onGlobalLayout() {
 
@@ -125,35 +135,29 @@ public class AnimationHelper implements OnChildAttachedListener {
                         Log.d("IMPORTANT", "First visible pos: " + firstItemPosition);
                         Log.d("IMPORTANT", "Last visible pos: " + lastItemPosition);
 
-                        mCurrentLastVisibleChildPosition = lastItemPosition;
-
-                        Log.d("IMPORTANT", "Current last visible: " + mCurrentLastVisibleChildPosition);
-
                         mNumRows = (int) Math.ceil(((double) (lastItemPosition - firstItemPosition) / (double) mNumColumns));
 
                         mNumChildren = mRecyclerView.getChildCount();
 
-                        mTotalHeight = mRecyclerView.getLayoutManager().getHeight();
-
                         mTotalItems = mRecyclerView.getAdapter().getItemCount();
 
-                        Log.d("IMPORTANT", "Number of Rows: " + mNumRows);
+                        Log.d("Lustig", "Number of Rows: " + mNumRows);
 
-                        Log.d("IMPORTANT", "Number of Children: " + mNumChildren);
+                        Log.d("Lustig", "Number of Children: " + mNumChildren);
 
-                        Log.d("IMPORTANT", "Number of Items: " + mTotalItems);
+                        Log.d("Lustig", "Number of Items: " + mTotalItems);
 
                         mViewsToAnimate = new View[mNumRows][mNumColumns];
 
-                        for (int row = 0; row < mNumRows; ++row) {
-
-                            for (int column = 0; column < mNumColumns; ++column) {
-
-                                int viewNumber = (row * (mNumColumns) + column);
-
-                                mViewsToAnimate[row][column] = mRecyclerView.getChildAt(viewNumber);
-                            }
-                        }
+//                        for (int row = 0; row < mNumRows; ++row) {
+//
+//                            for (int column = 0; column < mNumColumns; ++column) {
+//
+//                                int viewNumber = (row * (mNumColumns) + column);
+//
+//                                mViewsToAnimate[row][column] = mRecyclerView.getChildAt(viewNumber);
+//                            }
+//                        }
 
                         mTimeIntervals = mNumColumns + mNumRows - 2;
 
@@ -169,6 +173,16 @@ public class AnimationHelper implements OnChildAttachedListener {
     }
 
     private void animateAllViews() {
+
+        for (int row = 0; row < mNumRows; ++row) {
+
+            for (int column = 0; column < mNumColumns; ++column) {
+
+                int viewNumber = (row * (mNumColumns) + column);
+
+                mViewsToAnimate[row][column] = mRecyclerView.getChildAt(viewNumber);
+            }
+        }
 
         for (int T = 0; T <= (mTimeIntervals); T++) {
 
@@ -203,6 +217,8 @@ public class AnimationHelper implements OnChildAttachedListener {
 
         }
 
+        mAnimStartTime = System.currentTimeMillis();
+
         final Handler animDoneHandler = new Handler();
 
         animDoneHandler.postDelayed(
@@ -213,8 +229,14 @@ public class AnimationHelper implements OnChildAttachedListener {
                         Log.d("Lustig", "First run of anims completed");
 
                         mFirstAnimationsCompleted = true;
+
+                        mAnimEndTime = System.currentTimeMillis();
+
+                        mTimeToCompleteScreenAnimation = mAnimEndTime - mAnimStartTime;
+
+                        Log.d("Lustig", "Screen animation completed in " + mTimeToCompleteScreenAnimation + "ms");
                     }
-                }, mDeltaT * mTimeIntervals
+                }, mDeltaT * (mTimeIntervals + 1)
         );
 
     }
@@ -263,10 +285,5 @@ public class AnimationHelper implements OnChildAttachedListener {
         }
 
     }
-
-
-
-
-
 
 }
